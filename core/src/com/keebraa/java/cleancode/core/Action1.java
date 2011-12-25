ba46f84237a8263c318f3324dc1c4094d58f6631
taqi <keebraa@gmail.com>
2011-12-26 00:22:14 +0200

    I found the solution to get all revisions. Congratulations! )))

--- core/src/com/keebraa/java/cleancode/core/Action1.java
+++ core/src/com/keebraa/java/cleancode/core/Action1.java
@@ -3,19 +3,14 @@ package com.keebraa.java.cleancode.core;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.team.core.RepositoryProvider;
-import org.eclipse.team.core.history.IFileHistoryProvider;
-import org.eclipse.team.ui.TeamUI;
 import org.eclipse.ui.IActionDelegate;
 
 import com.vectrace.MercurialEclipse.history.MercurialHistory;
 import com.vectrace.MercurialEclipse.history.MercurialRevision;
-import com.vectrace.MercurialEclipse.team.MercurialTeamProvider;
-import com.vectrace.MercurialEclipse.utils.ResourceUtils;
 
 public class Action1 implements IActionDelegate
 {
@@ -28,6 +23,21 @@ public class Action1 implements IActionDelegate
     @Override
     public void run(IAction action)
     {
+	MercurialHistory history = new MercurialHistory(project);
+	try
+	{
+	    history.refresh(null, Integer.MAX_VALUE);
+	    List<MercurialRevision> revisions = history.getRevisions();
+	    for(MercurialRevision revision : revisions)
+	    {
+		System.out.println(revision.getComment());
+	    }
+	}
+	catch (CoreException e)
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
 //        RepositoryProvider provider = RepositoryProvider.getProvider(project);
 //        MercurialTeamProvider mProvider = (MercurialTeamProvider) provider;
 //        MercurialHistory history = (MercurialHistory)mProvider.getFileHistoryProvider().getFileHistoryFor(project, IFileHistoryProvider.NONE, null);
