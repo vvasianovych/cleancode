9a139b9206f465199bd52aa18098d3e489c9120c
Viacheslav Vasianovych <keebraa@gmail.com>
2012-01-24 00:25:41 +0200

    CLOSED - task 36: implement fillCodeReviewBuilder method in the firstWizardPage http://github.com/taqi/cleancode/issues/issue/36

--- cleancode-core/src/com/keebraa/java/cleancode/core/reviewcreation/wizard/pages/commitpage/CommitSelectionListener.java
+++ cleancode-core/src/com/keebraa/java/cleancode/core/reviewcreation/wizard/pages/commitpage/CommitSelectionListener.java
@@ -1,10 +1,8 @@
 package com.keebraa.java.cleancode.core.reviewcreation.wizard.pages.commitpage;
 
-import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 
 /**
  * This listener listening CommitTable from the first Wizard page and setting
@@ -16,37 +14,27 @@ import org.eclipse.swt.widgets.TableItem;
 public class CommitSelectionListener implements SelectionListener
 {
 
-   @Override
-   public void widgetSelected(SelectionEvent event)
-   {
-	Object obj = event.getSource();
-	if (!(obj instanceof Table))
-	{
-	   return;
-	}
-	Table table = (Table) obj;
-	boolean selected = false;
-	for (TableItem item : table.getItems())
-	{
-	   table.getParent().getParent();
-	   if (item.getChecked())
-	   {
-		selected = true;
-		break;
-	   }
-	}
-	Object pageObject = table.getData();
-	if(!(pageObject instanceof WizardPage))
-	{
-	   return;
-	}
-	WizardPage page = (WizardPage) pageObject;
-	page.setPageComplete(selected);
-   }
+    @Override
+    public void widgetSelected(SelectionEvent event)
+    {
+        Object obj = event.getSource();
+        if (!(obj instanceof Table))
+        {
+            return;
+        }
+        Table table = (Table) obj;
+        Object pageObject = table.getData();
+        if (!(pageObject instanceof CommitSelectionWizardPage))
+        {
+            return;
+        }
+        CommitSelectionWizardPage page = (CommitSelectionWizardPage) pageObject;
+        page.setPageComplete(!page.getSelectedCommits().isEmpty());
+    }
 
-   @Override
-   public void widgetDefaultSelected(SelectionEvent event)
-   {
-	widgetSelected(event);
-   }
+    @Override
+    public void widgetDefaultSelected(SelectionEvent event)
+    {
+        widgetSelected(event);
+    }
 }
